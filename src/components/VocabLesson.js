d052204623bb8605cd9a855ea55d4ebc0479f903
Raitokomniak <raitokomniak@gmail.com>
2023-11-09 19:19:22 +0200

    working state machine

--- src/components/VocabLesson.js
+++ src/components/VocabLesson.js
@@ -5,81 +5,76 @@ import { random } from 'node-forge';
 
 
 const VocabLesson = () => {
-    let [qIndex, setIndex] = useState(0);
-    let [qState, setQState] = useState(0);
-    let choiceElements = [];
+    const [qIndex, setIndex] = useState(0);
+    const [lesson, setLesson] = useState(null)
+    const [qState, setQState] = useState(0);
+    const choiceElements = [];
 
-        const lesson = new Lesson("Lesson1"); //TESTI
 
-        const qWord = () => {
-            return (
-                <div className="wordcontainer">{lesson.wordList[qIndex]}</div>
-            )
-        }
 
-        const endQuiz = () => {
-            if(lesson.wordList.length != 0 && qIndex >= lesson.wordList.length) return true
-            else return false;
-        }
+    const endQuiz = () => {
+        if (lesson.wordList.length != 0 && qIndex >= lesson.wordList.length) return true
+        else return false;
+    }
 
-        const handleChoice = (e) => {
-            console.log("choice " + e);
-        };
+    const handleChoice = (e) => {
+        console.log("choice " + e);
+    };
 
-        const handleSkip = () => {
-            console.log("skip");
-            setIndex(qIndex + 1);
-            console.log("qindex " + qIndex);
-        };
+    const handleSkip = () => {
+        console.log("skip");
+        setIndex(qIndex + 1);
+        console.log("qindex " + qIndex);
+    };
 
-        const randNumber = () => {
-            return Math.floor(Math.random() * (lesson.wordList.length - 1 + 1)) + 0;
-        }
+    const randNumber = () => {
+        return Math.floor(Math.random() * (lesson.wordList.length - 1 + 1)) + 0;
+    }
 
-        const createRandomizedQuizOrder = () => {
-            for(var i = 0; i < lesson.wordList.length; i++){
-                const j = Math.floor(Math.random() * (i + 1));
+    const createRandomizedQuizOrder = () => {
+        for (var i = 0; i < lesson.wordList.length; i++) {
+            const j = Math.floor(Math.random() * (i + 1));
             [lesson.wordList[i], lesson.wordList[j]] = [lesson.wordList[j], lesson.wordList[i]];
             [lesson.translationList[i], lesson.translationList[j]] = [lesson.translationList[j], lesson.translationList[i]];
-            }
-
-            console.log(lesson.wordList);
-            console.log(lesson.translationList);
-            
-            setQState(1);
         }
 
-        const createWrongAnswers = (exclude) => {
-            console.log("createWrongAnswers()")
-            let indexes = [];
-            
-            for(var i = 0; i < 3; i++){
-                let randIndex = randNumber();
-    
-                while(indexes.includes(randIndex) || randIndex == exclude) { 
-                    randIndex = randNumber();
-                }
-                indexes.push(randIndex);
+        console.log(lesson.wordList);
+        console.log(lesson.translationList);
+
+
+    }
+
+    const createWrongAnswers = (exclude) => {
+        console.log("createWrongAnswers()")
+        let indexes = [];
+
+        for (var i = 0; i < 3; i++) {
+            let randIndex = randNumber();
+
+            while (indexes.includes(randIndex) || randIndex == exclude) {
+                randIndex = randNumber();
             }
-    
-            return indexes;
-          }
+            indexes.push(randIndex);
+        }
 
-        const createChoices = () => {
+        return indexes;
+    }
 
-            choiceElements.push(
-                <div key={"button0"} className='row'>
-                    <div className="col-md-10 text-center">
-                        <button className="btn choice-button w-100 text-center" onClick={() => handleChoice(0)}>{lesson.translationList[qIndex][0]}
-                        </button>
-                    </div>
+    const createChoices = () => {
+
+        choiceElements.push(
+            <div key={"button0"} className='row'>
+                <div className="col-md-10 text-center">
+                    <button className="btn choice-button w-100 text-center" onClick={() => handleChoice(0)}>{lesson.translationList[qIndex][0]}
+                    </button>
                 </div>
-                );
-    
-            let wrongAnswers = createWrongAnswers(qIndex);
-    
-            for(var i = 0; i < 3; i++){
-                (function (index) {
+            </div>
+        );
+
+        let wrongAnswers = createWrongAnswers(qIndex);
+
+        for (var i = 0; i < 3; i++) {
+            (function (index) {
                 choiceElements.push(
                     <div key={"button" + (i + 1)} className='row'>
                         <div className="col-md-10 text-center">
@@ -87,42 +82,48 @@ const VocabLesson = () => {
                             </button>
                         </div>
                     </div>
-                    );
-                })(i);
-            }
-    
-          }
-    
-        if(qState == 0) {createRandomizedQuizOrder()}
-        else { 
-            if(!endQuiz()) createChoices()
+                );
+            })(i);
         }
 
-        
-
-
-      
-
-    
-    if(!endQuiz()){
-  return (
-    <div className='container-fluid '>
-        <h1 align="center">{lesson.lessonName}</h1>
-
-        <div className='row justify-content-center align-items-center'>  
-            <div className='col-2'>{qIndex + 1} / {lesson.wordList.length}</div>
-            <div className='col-md-5'>{qWord()}</div>
-            <div className='col-md-3'><button className="btn skip-button w-100 text-center" onClick={() => handleSkip()}>Skip</button></div>
-        </div>
-        <div className='row justify-content-center'> 
-            <div className='col-md-4'>{choiceElements}</div>
-        </div>
-    </div>
-  );
     }
-    else {
-        return("voitit pelin");
+
+    console.log("qstate " + qState)
+    if (qState == 0 && lesson == null) {
+        console.log("here")
+        setLesson(new Lesson("Lesson1"));
+
     }
+    else if (qState == 0 && lesson != null) {
+        createRandomizedQuizOrder()
+        setQState(1);
+    }
+    else if (qState == 1) {
+        if (!endQuiz()) {
+            console.log("quizIndex " + qIndex + " word " + lesson.wordList[qIndex])
+            createChoices()
+
+
+            return (
+                <div className='container-fluid '>
+                    <h1 align="center">{lesson.lessonName}</h1>
+
+                    <div className='row justify-content-center align-items-center'>
+                        <div className='col-2'>{qIndex + 1} / {lesson.wordList.length}</div>
+                        <div className='col-md-5 wordcontainer'>{lesson.wordList[qIndex]}</div>
+                        <div className='col-md-3'><button className="btn skip-button w-100 text-center" onClick={() => handleSkip()}>Skip</button></div>
+                    </div>
+                    <div className='row justify-content-center'>
+                        <div className='col-md-4'>{choiceElements}</div>
+                    </div>
+                </div>
+            );
+        }
+        else {
+            return ("voitit pelin");
+        }
+    }
+
 };
 
 export default VocabLesson;
