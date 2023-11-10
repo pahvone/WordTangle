8690947f6cc0ce2d3a71466284a39fa3ee110844
Markus Risuaho <t1rima00@students.oamk.fi>
2023-11-10 14:54:19 +0200

    Scuffed barebones navbar for dashboard.

---
+++ src/components/NavBar.js
@@ -0,0 +1,40 @@
+import { NavLink } from 'react-router-dom'
+import './navbar.css'
+import logo from '../img/WTlogo_white_stroke.png'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+        <img src={logo} width={60} height={60} />
+        </div>
+        <div className="nav-elements">
+          <ul>
+            <li>
+              <NavLink to="/">DashBoard</NavLink>
+            </li>
+
+            <li>
+              <NavLink to="/blog">Learn</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Forums</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">Settings</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/signout">Sign Out</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
