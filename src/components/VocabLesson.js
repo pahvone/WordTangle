c15a414ef6fc504e89520ee59ac45b86448c9349
Raitokomniak <raitokomniak@gmail.com>
2023-11-10 17:36:15 +0200

    added typochecker to textinput, restructured quiz rendering, disabled wordlist/translationlist shuffle bc inputmode switch breaks it

--- src/components/VocabLesson.js
+++ src/components/VocabLesson.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback} from 'react';
 import Lesson from '../vocab/Vocab';
 import './VocabLesson.css';
-
 
 const VocabLesson = () => {
     const [qIndex, setIndex] = useState(0);
@@ -10,71 +9,104 @@ const VocabLesson = () => {
     const [result, setResult] = useState("")
     const [correctCount, setCorrectCount] = useState(0)
     const [inputMode, setInputMode] = useState(0)
-    const [textInput, setTextInput] = useState("")
+
+    const textInputRef = useRef(null)
+    const inputModeRef = useRef(null)
 
     const choiceElements = [];
 
-
+    
+    
     const endQuiz = () => {
         if (lesson.wordList.length != 0 && qIndex >= lesson.wordList.length) return true
         else return false;
     }
-    
-    const switchInputModeButton = () => {
 
-        if(inputMode == 0){
-            return(<button className="btn skip-button w-100 text-center" onClick={() => setInputMode(1)}>Answer in writing</button>)
-        }
-        else {
-            return(<button className="btn skip-button w-100 text-center" onClick={() => setInputMode(0)}>Multiple choice answers</button>)
-        }
-        
-    }
+    const handleSwitchInputMode = useCallback(() => {
+        setInputMode((prevInputModeRef) => (prevInputModeRef === 0 ? 1 : 0));
+    }, [setInputMode]);
 
     //Handles multiple choice answers
     const handleChoice = (index) => {
-        if(index == 0) handleResult(true)
-        else handleResult(false)
+        if (index == 0) handleResult("Correct")
+        else handleResult("Incorrect")
     };
 
     const handleResult = (result) => {
-        if(result == true) {
+        if (result == "Correct") {
             setResult("Correct!")
             setCorrectCount(correctCount + 1)
         }
-        else setResult("Incorrect. Correct answer is '" + lesson.translationList[qIndex][0] + "'")
+        else if (result == "Typoed") {
+            setResult("You might have a typo. You answered '" + textInputRef.current.value + "'. Correct answer is '" + lesson.translationList[qIndex][0] + "'")
+            setCorrectCount(correctCount + 1)
+        }
+        else if (result == "Incorrect")
+            setResult("Incorrect. Correct answer is '" + lesson.translationList[qIndex][0] + "'")
+
         setIndex(qIndex + 1);
     }
 
     //Handles text input answers
-    const handleTextInput = (event) => {
+    const handleTextInput = (event, qWordSwitch) => {
         event.preventDefault();
-        console.log(textInput + " vs " + lesson.translationList[qIndex][0])
 
-        if(textInput == lesson.translationList[qIndex][0] || (textInput == lesson.translationList[qIndex][1]))
-            handleResult(true);
-        else handleResult(false);
+        //later for iterating through all possible answers
+        /*for(var i = 0; i < lesson.translationList[qIndex].length; i++){
+            
+        }*/
+        let compareTo = "";
+        if (qWordSwitch == 0) compareTo = lesson.translationList[qIndex]
+        else compareTo = lesson.wordList[qIndex]
+        var correct = false;
 
-        setTextInput("")
+        if (Array.isArray(compareTo))
+            correct =  textInputRef.current.value == compareTo[0] || textInputRef.current.value == compareTo[1]
+        else 
+            correct = textInputRef.current.value == compareTo || textInputRef.current.value == compareTo
+            
+        if(!correct) {
+            //Check for typos
+            const input = textInputRef.current.value.split('');
+            let typoCompare = "";
+            if(Array.isArray(compareTo))
+                typoCompare = compareTo[0].split('');
+            else typoCompare = compareTo.split('');
+
+            let typoCount = 0;
+
+            for (let i = 0, j = 0; i < input.length && j < typoCompare.length; i++, j++) {
+                if (input[i] !== typoCompare[j]) {
+                    typoCount++;
+                    if (input.length > typoCompare.length) j--;
+                    else if (input.length < typoCompare.length)i--;
+                }
+                if (typoCount < 2) handleResult("Typoed");
+                if (typoCount > 1) handleResult("Incorrect");
+            }
+        }
+        else {
+            handleResult("Correct");
+        }
+
+        textInputRef.current.value = ""
         setIndex(qIndex + 1);
     }
 
     //Returns a text input form if inputMode == 1
-    const userInput = () => {
-        if(inputMode == 0) return choiceElements
+    const userInput = (qWordSwitch) => {
+        if (inputMode == 0) return choiceElements
         else return (
             <div>
                 <span className="align-middle">
                     <div className="formDiv">
-                    <form onSubmit={handleTextInput}>
-                        <p><input className="form-control form-control-lg" type="text" id="char" value={textInput} onChange={(e) => setTextInput(e.target.value)} required></input></p>
-                        <p><button type="submit" className="btn styled-button w-100">Submit</button></p>
-                    </form>
+                        <form onSubmit={(e) => handleTextInput(e, qWordSwitch)}>
+                            <p><input ref={textInputRef} className="form-control form-control-lg" type="text" id="char" required></input></p>
+                            <p><button type="submit" className="btn styled-button w-100">Submit</button></p>
+                        </form>
                     </div>
-                    </span>
+                </span>
             </div>
-
-            
         );
     }
 
@@ -95,19 +127,10 @@ const VocabLesson = () => {
             [lesson.translationList[i], lesson.translationList[j]] = [lesson.translationList[j], lesson.translationList[i]];
         }
 
-        for(var i = 0; i < lesson.wordList.length; i++){
-            if(randNumber() > 10){
-                var temp = lesson.wordList[i]
-                lesson.wordList[i] = lesson.translationList[i][0]
-                lesson.translationList[i][0] = temp
-            }
-        }
-
         setQState(1);
 
-       // console.log(lesson.wordList);
-       // console.log(lesson.translationList);
-
+        //console.log(lesson.wordList);
+        //console.log(lesson.translationList);
     }
 
     //Randomizes the order of the multiple choices
@@ -118,6 +141,7 @@ const VocabLesson = () => {
         }
     }
 
+    //Generate unique wrong answers from the rest of the wordlist
     const createWrongAnswers = (exclude) => {
         let indexes = [];
 
@@ -133,8 +157,50 @@ const VocabLesson = () => {
         return indexes;
     }
 
-    const createChoices = () => {
-        let word = lesson.translationList[qIndex][0];
+    const randomizeWordToTranslation = () => {
+
+    }
+
+    //Generate the currently quizzed word
+    const quizWord = () => {
+        
+        let qWordSwitch = 0;
+        var qWord = lesson.wordList[qIndex]
+        createChoices(qWordSwitch)
+
+        // randomly switch quizzed word to the other lang. 
+        // currently changing input mode breaks this because re-render
+        /*
+        let qWordSwitch = Math.floor(Math.random() * 2);
+        console.log("quizword() " + qWordSwitch);
+        createChoices(qWordSwitch)
+        var qWord = ""
+
+    
+        if (qWordSwitch == 0) qWord = lesson.wordList[qIndex]
+        else qWord = lesson.translationList[qIndex][0]*/
+
+        return (
+            <div>
+                <div className='row justify-content-center align-items-center'>
+                    <div className='col-2'>{qIndex + 1} / {lesson.wordList.length}</div>
+                    <div className='col-md-5 wordcontainer'>{qWord}</div>
+                    <div className='col-md-3'><button className="btn skip-button w-100 text-center" onClick={() => handleSkip()}>Skip</button></div>
+                </div>
+                <div className='row my-5 justify-content-center'>
+                    <div className='col-md-4'>{userInput(qWordSwitch)}</div>
+                </div>
+            </div>
+        )
+    }
+
+    //Generate answer choices for the currently quizzed word
+    const createChoices = (qWordSwitch) => {
+        let word = "";
+        word = lesson.translationList[qIndex][0];
+
+        if (qWordSwitch == 0) word = lesson.translationList[qIndex][0];
+        else if (qWordSwitch == 1) word = lesson.wordList[qIndex];
 
         choiceElements.push(
             <div key={"button0"} className='row'>
@@ -146,9 +212,11 @@ const VocabLesson = () => {
         );
 
         let wrongAnswers = createWrongAnswers(qIndex);
-        
+
         for (var i = 0; i < 3; i++) {
             word = lesson.translationList[wrongAnswers[i]][0];
+            if (qWordSwitch == 0) word = lesson.translationList[wrongAnswers[i]][0];
+            else if (qWordSwitch == 1) word = lesson.wordList[wrongAnswers[i]];
 
             (function (index) {
                 choiceElements.push(
@@ -163,17 +231,15 @@ const VocabLesson = () => {
         }
 
         createRandomizedChoiceOrder(choiceElements);
-
-       // console.log(choiceElements)
-
     }
+
 
     //////////////////////////////////////////////////////////
 
     //Quiz state machine
 
     if (qState == 0 && lesson == null) {
-        setLesson(new Lesson("FIN",1));
+        setLesson(new Lesson("FIN", 1));
     }
     else if (qState == 0 && lesson != null) {
         createRandomizedQuizOrder()
@@ -181,38 +247,41 @@ const VocabLesson = () => {
     else if (qState == 1) {
         if (!endQuiz()) {
             //console.log("quizIndex " + qIndex + " word " + lesson.wordList[qIndex])
-            createChoices()
 
             return (
                 <div className='container-fluid '>
                     <h1 align="center">{lesson.lessonName}</h1>
 
+                    {quizWord()}
                     <div className='row justify-content-center align-items-center'>
-                        <div className='col-2'>{qIndex + 1} / {lesson.wordList.length}</div>
-                        <div className='col-md-5 wordcontainer'>{lesson.wordList[qIndex]}</div>
-                        <div className='col-md-3'><button className="btn skip-button w-100 text-center" onClick={() => handleSkip()}>Skip</button></div>
-                    </div>
-                    <div className='row justify-content-center'>
-                        <div className='col-md-4'>{userInput()}</div>
-                    </div>
-                    <div className='row justify-content-center'>
                         <div className='col-md-4'>{result}</div>
                     </div>
                     <div className='row my-5 justify-content-center'>
-                        <div className='col-md-4'>{switchInputModeButton()}</div>
+                        <div className='col-md-4'> <button className="btn skip-button w-100 text-center" onClick={handleSwitchInputMode}>
+                            {inputMode === 0 ? 'Answer in writing' : 'Multiple choice answers'}
+                        </button></div>
                     </div>
                 </div>
             );
         }
         else {
             return (
-            <div className='container-fluid '>
-                <div className='row justify-content-center align-items-center'>
-                    <div className='col-md-4'>
-                    You got {correctCount} out of {lesson.wordList.length} correct
+                <div className='container-fluid '>
+                    <div className='row justify-content-center align-items-center'>
+                        <div className='col-md-4'>
+                            You got {correctCount} out of {lesson.wordList.length} correct
+                        </div>
+
+
                     </div>
-                </div>
-            </div>);
+                    <div className='row justify-content-center align-items-center'>
+                        <div className='col-md-4'>
+                            <button className="btn choice-button w-100 text-center" onClick={() => { }}>Back to dashboard
+                            </button>
+                        </div>
+                    </div>
+
+                </div>);
         }
     }
 
