54ac0ac914a55d84698230eb022daf39ec95ac9f
Raitokomniak <Raitokomniak@users.noreply.github.com>
2023-11-15 13:26:32 +0000

    Prettified Code!

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ const App = () => {
         <Route path="/Forums" element={<Forums />} />
         <Route path="/About" element={<About />} />
         <Route path="/UsernameChange" element={<UsernameChange />} />
-        <Route path="/LessonPath" element={<LessonPath/>}/>
-        <Route path="/LessonPage" element={<LessonPage/>}/>
+        <Route path="/LessonPath" element={<LessonPath />} />
+        <Route path="/LessonPage" element={<LessonPage />} />
       </Routes>
     </>
   );
