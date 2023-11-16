3f61c59924efd1a76d714483782895c12ca915e4
t1rima00 <markusveikka@gmail.com>
2023-11-16 15:05:49 +0200

    Enhanced the dashboard page, still barebones and hardcoded data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -35,18 +35,130 @@ function GetUserData() {
     });
 }
 
+const Progress_bar = ({progress}) => {
+    
+  const Parentdiv = {
+      height: 35,
+      width: '75%',
+      backgroundColor: '#DDDDDD',
+      borderRadius: 40,
+    }
+   
+    const Childdiv = {
+      height: '100%',
+      width: `${progress}%`,
+      backgroundColor: '#50FFC0',
+     borderRadius:40,
+    }
+   
+    const progresstext = {
+      padding: 10,
+      color: 'black',
+    }
+     
+  return (
+  <div style={Parentdiv}>
+    <div style={Childdiv}>
+      <span style={progresstext}>{`${progress}%`}</span>
+    </div>
+  </div>
+  )
+}
+
+
 const DashBoard = () => {
   return (
     <div>
       <NavBar />
-      <div className="responsive-container">
+      <div className="dashboardlogo-container">
         <img
           className="app-logo-nonstacked"
           src={nonstackedlogo}
           alt="Word Tangle Logo"
         />
-        {<Button text="Get User Data" onClick={GetUserData} />}
+        <span className="dashboardslogan">Your #1 language learning app</span>
       </div>
+
+      <div className="dashboardelements">
+
+        <div className="boxcontainer">
+        <div className="greycontainer">
+          <div className="title">Hi User!</div>
+          <div className="dashline"/>
+          <div className="latestactivity">Latest activity:</div>
+          <div className="activity">{">"} Completed a quiz
+          <span className="xp">25XP</span>
+          </div>
+          <div className="activity">{">"} Played a mini-game
+          <span className="xp">15XP</span>
+          </div>
+          <div className="activity">{">"} Posted in the forums
+          <span className="xp">10XP</span>
+          </div>
+        </div>
+        </div>
+
+        <div className="boxcontainer">
+        <div className="greycontainer">
+          <div className="title">DAILY TASKS</div>
+          <div className="dashline"/>
+          <div className="dailytask"> Completed a quiz
+          <span className="xp">10XP</span>
+          </div>
+          <div className="dailytask"> Played a mini-game
+          <span className="xp">10XP</span>
+          </div>
+          <div className="dailytask"> Browse the dictionary
+          <span className="xp">10XP</span>
+          </div>
+          <div className="xpbar">
+          <Progress_bar
+                progress="40"/>
+            </div>
+        </div>
+        </div>
+      </div> 
+      
+      <div className="dashboardelements">
+      <div className="boxcontainer">
+        <div className="greycontainer">
+          <div className="title">CONTINUE LEARNING</div>
+          <div className="dashline"/>
+          <div className="latestactivity">Latest activity:</div>
+          <div className="activity">{">"} Completed a quiz
+          <span className="xp">25XP</span>
+          </div>
+          <div className="activity">{">"} Played a mini-game
+          <span className="xp">15XP</span>
+          </div>
+          <div className="activity">{">"} Posted in the forums
+          <span className="xp">10XP</span>
+          </div>
+        </div>
+        </div>
+        <div className="boxcontainer">
+        <div className="greycontainer">
+          <div className="title">LEADERBOARDS
+          </div>
+          <span className="XPgained">
+            {"("}XP gained during the last 7 days{")"}
+          </span>
+          <div className="dashline"/>
+          <div className="leaderlist"> 1. Kyle
+          <span className="xp">25XP</span>
+          </div>
+          <div className="leaderlist"> 2. Eric
+          <span className="xp">15XP</span>
+          </div>
+          <div className="leaderlist"> 3. Stan
+          <span className="xp">10XP</span>
+          </div>
+          <div className="leaderlist"> 3. Juu
+          <span className="xp">10XP</span>
+          </div>
+        </div>
+        </div>
+        </div>
     </div>
   );
 };
