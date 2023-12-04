1d3dfe377ba58b3021fc403e78148e410ee66af7
pahvone <ville.pelimanni@gmail.com>
2023-12-04 22:12:43 +0200

    barebones shoutbox functionality added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Welcome from "./components/Welcome";
 import Settings from "./components/Settings";
-import Forums from "./components/Forums";
+import Forums from "./components/Shoutbox";
 import About from "./components/About";
 import UsernameChange from "./components/UsernameChange";
 import LessonPath from "./components/LessonPath";
 import LessonPage from "./components/LessonPage";
 import PasswordChange from "./components/PasswordChange";
 import Dashboard from "./components/Dashboard";
+import Shoutbox from "./components/Shoutbox";
 
 /* NOTE THAT GHPAGES REQUIRES ROUTING LIKE 
   <Route path="wordtangle/DashBoard" element={<Dashboard />} /> */
@@ -24,7 +25,7 @@ const App = () => {
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Settings" element={<Settings />} />
-        <Route path="/Forums" element={<Forums />} />
+        <Route path="/Shoutbox" element={<Shoutbox />} />
         <Route path="/About" element={<About />} />
         <Route path="/UsernameChange" element={<UsernameChange />} />
         <Route path="/PasswordChange" element={<PasswordChange />} />
---
+++ src/components/Shoutbox.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import fb from '../firebase';
+import NavBar from "./NavBar";
+import "./Settings.css";
+import { child, get, getDatabase, ref, remove, serverTimestamp } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import nonstackedlogo from "../img/wtlogo_nonstacked.png";
+import Footer from "./Footter";
+import { ServerTimestamp } from 'firebase/database';
+
+
+
+const auth = getAuth();
+const db = getDatabase(fb);
+const current_time = serverTimestamp();
+
+const Shoutbox = () => {
+  const [shouts, setShouts] = useState([]);
+  const [newShout, setNewShout] = useState('');
+
+  useEffect(() => {
+    const shoutsRef = fb.database().ref('shouts');
+    shoutsRef.on('value', (snapshot) => {
+      const shoutsData = snapshot.val();
+      if (shoutsData) {
+        const shoutsArray = Object.values(shoutsData);
+        setShouts(shoutsArray);
+      }
+    });
+  }, []);
+
+  const handleInputChange = (e) => {
+    setNewShout(e.target.value);
+  };
+
+  const handleAddShout = () => {
+    const auth = getAuth();
+    const shoutsRef = fb.database().ref('shouts/');
+    const newShoutRef = shoutsRef.push();
+    newShoutRef.set({
+        user: auth.currentUser.uid,
+        message: newShout,
+        timestamp: current_time,
+    });
+    setNewShout('');
+  };
+
+  return (
+    <div>
+        <NavBar />
+        <div className="pagecontainer">
+            <div classname="responsive-container">
+                {shouts.map((shout) => (
+                <li key={shout.timestamp}>{shout.message}</li>
+                ))}
+            <p />
+            <input
+                type="text"
+                value={newShout}
+                onChange={handleInputChange}
+            />
+            <button onClick={handleAddShout}>Add Shout</button>
+            <p />
+            </div>
+        </div>
+    <Footer />
+    </div>
+  );
+};
+
+export default Shoutbox;
